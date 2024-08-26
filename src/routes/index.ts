196893c8bda30394287287052eddd98c50f20ae7
XiaoMouz <gxiaomouz@gmail.com>
2024-08-26 23:34:22 +0800

    chore: restful

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,13 @@
 export default defineEventHandler((event) => {
+  // 返回可用的路由
+  //get visit host
+  const host = getHeader(event, 'host')
+  // get protocol
+  const protocol = getHeader(event, 'x-forwarded-proto')
   return {
-    message: 'OK',
+    'node record': {
+      'all node test records': `${protocol}://${host}/node/record`,
+      'node test record': `${protocol}://${host}/node/record/:id`,
+    },
   }
 })
---
+++ src/routes/node/record/[id]/index.get.ts
@@ -0,0 +1,9 @@
+import { getValue } from '~/model/kv'
+import { Result } from '~/types/speedtest'
+
+export default defineEventHandler(async (event) => {
+  const id = getRouterParam(event, 'id')
+  return (await getValue<Result[]>('test-result'))?.find(
+    (result) => result.id === id
+  )
+})
