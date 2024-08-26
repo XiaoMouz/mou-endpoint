28cb0705477674d32d0c910237eb36b0bb9404e2
XiaoMouz <gxiaomouz@gmail.com>
2024-08-26 22:28:20 +0800

    fix: no ssl fetch,and reset password

--- src/routes/init.ts
+++ src/routes/init.ts
@@ -2,8 +2,13 @@ import { haveValue, setValue } from '~/model/kv'
 
 export default defineEventHandler(async (event) => {
   // have force and hash key can reset
+  const env = process.env
   const queryKey = getQuery(event)
-  if (queryKey.force === 'true' && queryKey.hash === 'hash') {
+  if (
+    queryKey.force === 'true' &&
+    queryKey.hash === env.resetHash &&
+    env.resetHash
+  ) {
     await setValue('test-result', JSON.stringify([]))
     return {
       message: 'OK',
--- src/utils/network.ts
+++ src/utils/network.ts
@@ -2,21 +2,36 @@ import { type Node } from '~/types/node'
 import { type TestResult } from '~/types/test-result'
 
 export async function testNode(node: Node): Promise<TestResult> {
+  fetch(`http://${node.address}/`, {
+    method: 'GET',
+  })
   const start = Date.now()
-  return await fetch(`https://${node.address}/`, {
+  return fetch(`http://${node.address}/`, {
     method: 'GET',
   })
     .then((response) => {
       const end = Date.now()
       return {
         delay: end - start,
-        error: !response.ok,
+        error: false,
         status: 'up' as const,
         time: end,
       }
     })
     .catch((error) => {
       const end = Date.now()
+      if (
+        error.status === 403 ||
+        error.status === 404 ||
+        error.status === 500
+      ) {
+        return {
+          delay: end - start,
+          error: false,
+          status: 'up' as const,
+          time: end,
+        }
+      }
       return {
         delay: end - start,
         error: true,
