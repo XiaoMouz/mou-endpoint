5ee5cd6343ac00e08dfcf6089a4b52039786f21f
XiaoMouz <gxiaomouz@gmail.com>
2024-09-24 11:38:02 +0800

    chore: new route

---
+++ src/routes/me/index.ts
@@ -0,0 +1,13 @@
+import { H3EventFetch } from 'nitropack'
+
+export default defineEventHandler((evt) => {
+  return {
+    name: 'XiaoMouz',
+    snippets: [
+      'A Gamer',
+      'A Backend Developer',
+      'A Frontend Developer',
+      'An Android Developer',
+    ],
+  }
+})
