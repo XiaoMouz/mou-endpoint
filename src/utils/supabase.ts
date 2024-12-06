b5fa053f4dff656118e94b2eac0895c33a19f3be
XiaoMouz <gxiaomouz@gmail.com>
2024-12-06 21:47:27 +0800

    ci(env): throw missing env

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from '~/types/database.types'
 const env = process.env
 if (!env.SUPABASE_URL || !(env.SUPABASE_KEY && env.SUPABASE_SERVICE_KEY)) {
-  throw new Error('Missing env variables for Supabase')
+  throw new Error('Missing env variables for Supabase' + JSON.stringify(env))
 }
 const supbase = createClient<Database>(
   env.SUPABASE_URL,
