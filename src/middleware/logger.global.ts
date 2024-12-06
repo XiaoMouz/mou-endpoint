e8e55ac91cab1e1fd5d80bc727761a6f14e18788
XiaoMouz <gxiaomouz@gmail.com>
2024-12-06 16:57:09 +0800

    fix(log): cannot got requester ip

--- src/middleware/logger.global.ts
+++ src/middleware/logger.global.ts
@@ -4,6 +4,7 @@ export default defineEventHandler(async (event) => {
   let message = `[INFO] [${event.method}] From: @${
     event.node.req.headers['x-forwarded-for'] ||
     event.node.req.socket.remoteAddress ||
+    event.node.req.headers['CF-Connecting-IP'] ||
     'Unknown'
   } To (${event.node.req.url})`
   console.log(message)
