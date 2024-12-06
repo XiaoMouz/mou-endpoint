6a6105f66755b00ae9e79a9f186dadf24f8031ca
XiaoMouz <gxiaomouz@gmail.com>
2024-12-06 21:57:01 +0800

    fix(sdk): supabase single entity

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,14 +1,20 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '~/types/database.types'
-const env = process.env
-if (!env.SUPABASE_URL || !(env.SUPABASE_KEY && env.SUPABASE_SERVICE_KEY)) {
-  throw new Error('Missing env variables for Supabase' + JSON.stringify(env))
-}
-const supbase = createClient<Database>(
-  env.SUPABASE_URL,
-  env.SUPABASE_SERVICE_KEY || env.SUPABASE_KEY
-)
 
+var supbase: any = null
 export function useClient() {
+  if (!supbase) {
+    const env = process.env
+    if (!env.SUPABASE_URL || !(env.SUPABASE_KEY && env.SUPABASE_SERVICE_KEY)) {
+      throw new Error(
+        'Missing env variables for Supabase' + JSON.stringify(env)
+      )
+    }
+    supbase = createClient<Database>(
+      env.SUPABASE_URL,
+      env.SUPABASE_SERVICE_KEY || env.SUPABASE_KEY
+    )
+  }
+
   return supbase
 }
